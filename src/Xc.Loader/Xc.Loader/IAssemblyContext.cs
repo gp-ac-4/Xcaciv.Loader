643c4fd41a2fabcd57c22c4571b85d6df4711f76
ac-4 <ac-4@users.noreply.github.com>
2021-12-18 13:34:22 -0700

    initial code

---
+++ src/Xc.Loader/Xc.Loader/IAssemblyContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace Xc.Loader
+{
+    /// <summary>
+    /// class for managing dynamic assembly loading
+    /// </summary>
+    public interface IAssemblyContext
+    {
+        Assembly Assembly { get; }
+        string FilePath { get; }
+        List<string> NameSpaces { get; }
+        List<Type> Types { get; }
+    }
+}
---
+++ src/Xc.Loader/Xc.Loader/TypeContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xc.Loader
+{
+    /// <summary>
+    /// type utility for usage with IAssemblyContext
+    /// </summary>
+    public class TypeContext
+    {
+        /// <summary>
+        /// refrence to assembly context
+        /// </summary>
+        private IAssemblyContext context;
+        /// <summary>
+        /// context for validating assembly types
+        /// </summary>
+        /// <param name="context"></param>
+        public TypeContext(IAssemblyContext context)
+        {
+            this.context = context;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T? GetInstance<T>(string typename)
+        {
+            var instanceType = this.context.Types.FirstOrDefault(T => T.FullName?.EndsWith(typename)??false);
+            return (instanceType == null) ? default : (T)Activator.CreateInstance(instanceType);
+        }
+
+    
+    }
+}
