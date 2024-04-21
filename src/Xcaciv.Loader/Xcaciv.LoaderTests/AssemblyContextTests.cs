eb80d63dfb7e32995035bc75c056c17cfe207432
Xcaciv <23732819+Xcaciv@users.noreply.github.com>
2024-04-21 16:52:07 -0600

    making changes to tests loading

--- src/Xcaciv.Loader/Xcaciv.LoaderTests/AssemblyContextTests.cs
+++ src/Xcaciv.Loader/Xcaciv.LoaderTests/AssemblyContextTests.cs
@@ -1,36 +1,51 @@
 using Xunit;
-using Xcaciv.Loader;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using zTestInterfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Xcaciv.Loader.Tests
 {
     public class AssemblyContextTests
     {
-        private string simpleDllPath = @"..\..\..\..\TestAssembly\bin\Debug\net8.0\zTestAssembly.dll";
-        private string dependentDllPath = @"..\..\..\..\zTestDependentAssembly\bin\Debug\net8.0\zTestDependentAssembly.dll";
+        private TestContext _testContext;
+        private string simpleDllPath = @"..\..\..\..\TestAssembly\bin\{1}\net8.0\zTestAssembly.dll";
+        private string dependentDllPath = @"..\..\..\..\zTestDependentAssembly\bin\{1}\net8.0\zTestDependentAssembly.dll";
+
+        public AssemblyContextTests(TestContext testContext)
+        {
+            this._testContext = testContext;
+#if DEBUG
+            this._testContext.WriteLine("Tests in Debug mode");
+            simpleDllPath = simpleDllPath.Replace("{1}", "Debug");
+            dependentDllPath = dependentDllPath.Replace("{1}", "Debug");
+#else
+            this._testContext.WriteLine("Tests in Release mode");
+            simpleDllPath = simpleDllPath.Replace("{1}", "Release");
+            dependentDllPath = dependentDllPath.Replace("{1}", "Release");
+#endif
+        }
 
         [Fact()]
         public void VerifyPath_Test()
         {
             var restrictedPath = System.IO.Path.Combine("C:", "some", "folder", "path");
             var filePath = System.IO.Path.Combine(restrictedPath, "subpath");
-            var actualpath = AssemblyContext.VerifyPath(filePath);
+            var actualpath = Xcaciv.Loader.AssemblyContext.VerifyPath(filePath);
 
-            Assert.Equal(filePath, actualpath);
+            Xunit.Assert.Equal(filePath, actualpath);
         }
 
         [Fact()]
         public void LoadAssembly_Test()
         {
             var expectedPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, simpleDllPath));
-            var context = new AssemblyContext(simpleDllPath);
-            
-            Assert.Equal(expectedPath, context.FilePath);       
+            var context = new Xcaciv.Loader.AssemblyContext(simpleDllPath);
+
+            Xunit.Assert.Equal(expectedPath, context.FilePath);       
         }
 
 
@@ -39,7 +54,7 @@ namespace Xcaciv.Loader.Tests
         {
             var actual = String.Empty;
 
-            using (var context = new AssemblyContext(simpleDllPath))
+            using (var context = new Xcaciv.Loader.AssemblyContext(simpleDllPath))
             {
                 IClass1? class1 = context.GetInstance("Class1") as IClass1;
                 actual = class1?.Stuff("input text here") ?? String.Empty;
@@ -49,12 +64,12 @@ namespace Xcaciv.Loader.Tests
             GC.Collect();
             GC.WaitForPendingFinalizers();
 
-            Assert.Equal("input text here output", actual);
+            Xunit.Assert.Equal("input text here output", actual);
         }
 
         private string UseFactory(string path)
         {
-            using (var context = AssemblyContext.LoadFromPath(path))
+            using (var context = Xcaciv.Loader.AssemblyContext.LoadFromPath(path))
             {
                 IClass1? class1 = context.GetInstance("Class1") as IClass1;
                 return class1?.Stuff("input text here") ?? String.Empty;
@@ -70,7 +85,7 @@ namespace Xcaciv.Loader.Tests
             GC.Collect();
             GC.WaitForPendingFinalizers();
 
-            Assert.Equal("input text here output", actual);
+            Xunit.Assert.Equal("input text here output", actual);
         }
 
         public string DoManual(string aPath)
@@ -91,38 +106,38 @@ namespace Xcaciv.Loader.Tests
         [Fact()]
         public void ManualTest()
         {
-            var actual = DoManual(AssemblyContext.VerifyPath(simpleDllPath));
+            var actual = DoManual(Xcaciv.Loader.AssemblyContext.VerifyPath(simpleDllPath));
 
             GC.Collect();
             GC.WaitForPendingFinalizers();
 
-            Assert.Equal("input text here output", actual);
+            Xunit.Assert.Equal("input text here output", actual);
         }
 
         [Fact()]
         public void UsingFactory_WithDependency_Unloads()
         {
             var actual = String.Empty;
-            using (var context = AssemblyContext.LoadFromPath(dependentDllPath))
+            using (var context = Xcaciv.Loader.AssemblyContext.LoadFromPath(dependentDllPath))
             {
                 IClass1? class1 = context.GetInstance("Class1") as IClass1;
                 actual = class1?.Stuff("input text here") ?? String.Empty;
             }
 
-            Assert.Equal("5,5,8", actual);
+            Xunit.Assert.Equal("5,5,8", actual);
         }
 
         [Fact()]
         public void UsingStrongTypedFactory_Unloads()
         {
             var actual = String.Empty;
-            using (var context = AssemblyContext.LoadFromPath(dependentDllPath))
+            using (var context = Xcaciv.Loader.AssemblyContext.LoadFromPath(dependentDllPath))
             {
                 var class1 = context.GetInstance<IClass1>("Class1");
                 actual = class1?.Stuff("input text here") ?? String.Empty;
             }
 
-            Assert.Equal("5,5,8", actual);
+            Xunit.Assert.Equal("5,5,8", actual);
         }
 
     }
